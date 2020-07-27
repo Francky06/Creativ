57c1bf16e2f69f0a1f47efa3a429cf1bfcc59c03
Francky06 <ffroswell@gmx.fr>
2020-07-27 18:05:00 +0200

    ajout video, refonte complete

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ c.addEventListener("dblclick", function() {
 	c.innerHTML = "Sheldon Cooper"
 });
 let score = 0;
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button1");
 for (let i = 0; i < buttons.length; i++) {
 buttons[i].addEventListener("click", function() {  
 const joueur = buttons[i].innerHTML;
